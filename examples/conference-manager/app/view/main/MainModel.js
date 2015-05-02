75ee0a5ea22f39f4b03f36dec5d77b7c8b081989
Steffen Hiller <s.hiller@360releases.com>
2015-05-02 10:44:38 +0200

    Added new conference-manager app to package examples with "sencha generate app CM conference-manager".

---
+++ examples/conference-manager/app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('CM.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'CM'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
